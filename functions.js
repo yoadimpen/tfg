eb57acee0f256e92170f6b77c853407d532afcf3
Yoana Penkova <yoadimpen@alum.us.es>
2020-12-28 15:44:21 +0100

    ST014/004: pagination is done and style is applied

--- functions.js
+++ functions.js
@@ -1,109 +1,40 @@
-/*import * as fs from 'fs';
-import * as path from 'path';
-//Given a path input, it should search for the summary JSON file in the allure-report
-function searchForReportsNODE(){
-
-    //this should be the input with the folder containing all the allure-report folders
-    var givenPath = "F:\- TFG\code\scrap-code\Allure-Multidashboard-master";
-    var res = "";
-
-    const path = require('path');
-    const fs = require('fs');
-
-    fs.readdir(givenPath, function(err, files){
-        if(err) {
-            return console.log('Unable to scan directory: ' + err);
-        }
-
-        filesNumber = files.length;
-        i = 1;
-
-        files.forEach(function(file){
-            if(file.includes("allure-report")){
-                res.concat(file);
-            }
-            if(i != filesNumber){
-                res.concat(";");
-            }
-            i = i + 1;
-        });
-    });
-
-    return res;
-}*/
-
 function generateView(){
-    //uses the allure-report path to find the summary JSON and calls the readJSON function
-    //var reportsPath = "file:///F:/-TFG/code/scrap-code/Allure-Multidashboard-master/allure-report";
-        //var arrayPaths = reportsPath.split(";");
-        //arrayPaths.forEach(function(file){
     
     deleteWidgets();
 
     var reportsPath = document.getElementById("generateInput").value;
     var pathsArray = reportsPath.split(";");
-    //console.log(pathsArray.length);
+    
     pathsArray.forEach(function(path){
         path = path.trim();
-        //console.log(path);
         var fullPath = path.concat("/widgets/summaryCopy.json");
         readJSON(fullPath, path);
-        //console.log(document.getElementsByTagName("html")[0].outerHTML);
     });
-        //console.log(reportsPath);
-        //var fullPath = reportsPath.concat("/widgets/summaryCopy.json");
-        //return readJSON(fullPath, reportsPath);
-    //});
-    //document.getElementById("widgets").innerHTML += "Widgets have been generated correctly!";
-    
 }
 
-//Given a path of the JSON file, it should read it
 function readJSON (JSONFile, folderPath) {
-    //should 'return' the json object
-    //if it's not found it should call showNotFound
-
+    
     var request = new XMLHttpRequest();
     var data = "";
     var jobject = "";
 
     request.withCredentials = true;
     request.open('GET', JSONFile);
-    //request.setRequestHeader("Content-Type", "application/json");
+    
     request.overrideMimeType("application/json");
-    /*request.onreadystatechange = function(){
-        if(request.readyState == 4 && request.status == 0) {
-            console.log("aqui");
-            console.log(request.responseText);
-        }
-    };*/
+   
     request.send();
-    /*if(request.status == 0){
-        //should generate JSON Object
-        console.log(request.response);
-        jobject = JSON.parse(request.responseText);
-        generateWidget(jobject, folderPath);
-    } else {
-        console.log("something went wrong");
-        //should go to showNotFound
-    }*/
-
-    //request.addEventListener("readystatechange", function() {
+    
     request.onreadystatechange = function() {
         if(this.readyState === 4) {
-            console.log(this.responseText);
+            //console.log(this.responseText);
             data = request.responseText;
             jobject = JSON.parse(data);
             
-            console.log(jobject);
+            //console.log(jobject);
             generateWidget(jobject, folderPath);
         }
-         
-        //console.log(jobject);
-        //generateView(jobject, folderPath);
-        //console.log("coso");
     };
-    //return generateWidget(jobject, folderPath);
 }
 
 //No parameters should be given here
@@ -111,11 +42,9 @@ function showNotFound(){
     //should return a not found message, try again or something like that
 }
 
-//Given the JSON object, this should generate a new widget in the home html
 function generateWidget(jobject, folderPath){
-    //should return 1 if everything went well and 0 if there was an error in order to display a message
-    console.log(jobject.reportName);
-    console.log(jobject.statistic);
+    //console.log(jobject.reportName);
+    //console.log(jobject.statistic);
     var statistic = jobject.statistic;
     var reportName = jobject.reportName;
 
@@ -126,7 +55,7 @@ function generateWidget(jobject, folderPath){
 
     var total = statistic.total;
 
-    console.log(statistic.failed);
+    //console.log(statistic.failed);
 
     if(parseInt(total) == 0){
         failed1 = 0;
@@ -163,27 +92,23 @@ function generateWidget(jobject, folderPath){
     var ul = document.getElementById("widgets");
 
     var text1 = document.createElementNS("http://www.w3.org/2000/svg", "text");
-    //var text1 = document.createElement("text");
     text1.setAttribute("x", "50%");
     text1.setAttribute("y", "50%");
     text1.setAttribute("class", "chart-number")
     text1.appendChild(document.createTextNode(total));
 
     var text2 = document.createElementNS("http://www.w3.org/2000/svg", "text");
-    //var text2 = document.createElement("text");
     text2.setAttribute("x", "50%");
     text2.setAttribute("y", "50%");
     text2.setAttribute("class", "chart-label")
     text2.appendChild(document.createTextNode("tests"));
 
     var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
-    //var g = document.createElement("g");
     g.setAttribute("class", "chart-text");
     g.appendChild(text1);
     g.appendChild(text2);
 
     var circleHole = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    //var circleHole = document.createElement("circle");
     circleHole.setAttribute("class", "donut-hole");
     circleHole.setAttribute("cx", "21");
     circleHole.setAttribute("cy", "21");
@@ -192,7 +117,6 @@ function generateWidget(jobject, folderPath){
 
 
     var circleRing = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    //var circleRing = document.createElement("circle");
     circleRing.setAttribute("class", "donut-ring");
     circleRing.setAttribute("cx", "21");
     circleRing.setAttribute("cy", "21");
@@ -202,7 +126,6 @@ function generateWidget(jobject, folderPath){
     circleRing.setAttribute("stroke-width", "3");
 
     var circle1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    //var circle1 = document.createElement("circle");
     circle1.setAttribute("class", "donut-segment");
     circle1.setAttribute("cx", "21");
     circle1.setAttribute("cy", "21");
@@ -214,7 +137,6 @@ function generateWidget(jobject, folderPath){
     circle1.setAttribute("stroke-dashoffset", dashFailed);
 
     var circle2 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    //var circle2 = document.createElement("circle");
     circle2.setAttribute("class", "donut-segment");
     circle2.setAttribute("cx", "21");
     circle2.setAttribute("cy", "21");
@@ -226,7 +148,6 @@ function generateWidget(jobject, folderPath){
     circle2.setAttribute("stroke-dashoffset", dashBroken);
 
     var circle3 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    //var circle3 = document.createElement("circle");
     circle3.setAttribute("class", "donut-segment");
     circle3.setAttribute("cx", "21");
     circle3.setAttribute("cy", "21");
@@ -238,7 +159,6 @@ function generateWidget(jobject, folderPath){
     circle3.setAttribute("stroke-dashoffset", dashSkipped);
 
     var circle4 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    //var circle4 = document.createElement("circle");
     circle4.setAttribute("class", "donut-segment");
     circle4.setAttribute("cx", "21");
     circle4.setAttribute("cy", "21");
@@ -250,7 +170,6 @@ function generateWidget(jobject, folderPath){
     circle4.setAttribute("stroke-dashoffset", dashPassed);
 
     var circle5 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    //var circle5 = document.createElement("circle");
     circle5.setAttribute("class", "donut-segment");
     circle5.setAttribute("cx", "21");
     circle5.setAttribute("cy", "21");
@@ -261,7 +180,6 @@ function generateWidget(jobject, folderPath){
     circle5.setAttribute("stroke-dasharray", unknown);
     circle5.setAttribute("stroke-dashoffset", dashUnknown);
 
-    //var svg = document.createElement("svg");
     var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     svg.setAttribute("width", "100%");
     svg.setAttribute("height", "100%");
@@ -339,7 +257,6 @@ function generateWidget(jobject, folderPath){
 
 }
 
-//No parameters, this should delete all existing widgets on the page in order to generate a more updated ones
 function deleteWidgets(){
     document.getElementById("widgets").innerHTML = "";
 }
@@ -385,7 +302,6 @@ function getCorrectCriteria(arrayOfP, criteria){
     arrayOfP.forEach(function(p){
         var p2 = document.createElement("p");
         p2 = p;
-        //console.log(p2.getAttribute("id"));
         if(p2.getAttribute("id") === criteria){
             res = p2.innerText;
         }
@@ -396,14 +312,10 @@ function getCorrectCriteria(arrayOfP, criteria){
 
 function showOrderedWidgets(){
     var criteria = document.getElementById("orderBy").value;
-    //console.log(criteria);
     var divs = Array.prototype.slice.call(document.getElementsByClassName("apiSummary"));
-
-    //console.log(divs[0].innerText);
 
     function customSorter(a,b){
         var res = 0;
-        //console.log(a.innerText);
         var childrenA = Array.prototype.slice.call(a.childNodes);
         var childrenB = Array.prototype.slice.call(b.childNodes);
         
@@ -418,23 +330,17 @@ function showOrderedWidgets(){
                 Array.prototype.slice.call(childrenB[0].childNodes)[5],
                 Array.prototype.slice.call(childrenB[0].childNodes)[6]];
 
-        //console.log(Array.prototype.slice.call(childrenA[0].childNodes)[2]);
-
         specificPA = parseInt(getCorrectCriteria(pA, criteria));
         specificPB = parseInt(getCorrectCriteria(pB, criteria));
         if(specificPA > specificPB){
-            //console.log("el número " + specificPA + " es mayor que " + specificPB);
             res = -1;
         } else if (specificPA < specificPB){
-            //console.log("el número " + specificPA + " es menor que " + specificPB);
             res = 1;
         }
         return parseInt(res);
     }
 
     var sortedDivs = Array.prototype.slice.call(divs).sort(customSorter);
-
-    //console.log(sortedDivs[0].innerText);
 
     document.getElementById("widgets").innerHTML = "";
     var ul = document.getElementById("widgets");
@@ -445,14 +351,9 @@ function showOrderedWidgets(){
 }
 
 function doPagination(){
-    //get all widgets
-    //divide them into groups, these groups will be the pages
-    //establish 'display: none' to those who aren't on the current page
-    //---------------------------------------------------------------------
     
     var pages = makePages();
 
-    //add pages access to html
     addPagesAccessToHtml(pages.length);
 
 }
@@ -463,7 +364,7 @@ function makePages(){
 
     var n = parseInt(nElement);
 
-    console.log(n);
+    //console.log(n);
 
     var divs = Array.prototype.slice.call(document.getElementsByClassName("apiSummary"));
 
@@ -473,10 +374,8 @@ function makePages(){
 
     for (i = 0; i < divs.length; i=i+n) {
         if(i+n < divs.length){
-            //console.log("pagina entera");
             arrayOfPages[j] = divs.slice(i, i+n);
         } else {
-            //console.log("pagina final");
             arrayOfPages[j] = divs.slice(i, divs.length);
         }
         j = j + 1;
@@ -489,11 +388,11 @@ function addPagesAccessToHtml(nP){
     var paginationDiv = document.getElementById("paginationDiv");
     var pagesAccess;
 
-    //FALTA ARREGLAR LO DE LA REPETICIÓN DE BOTONES DE PÁGINAS
     if(document.getElementById("pages") == null){
         pagesAccess = document.createElement("div");
         pagesAccess.setAttribute("id", "pages");
     } else {
+        pagesAccess = document.getElementById("pages")
         document.getElementById("pages").innerHTML = "";
     }
 
@@ -508,6 +407,7 @@ function addPagesAccessToHtml(nP){
     }
 
     paginationDiv.appendChild(pagesAccess);
+    goToPage(0);
 
 }
 
@@ -520,15 +420,11 @@ function hideAllDivs(arrayOfDivs){
 function goToPage(i){
     var divs = Array.prototype.slice.call(document.getElementsByClassName("apiSummary"));
 
-    //console.log(divs);
-
     hideAllDivs(divs);
 
     var pages = makePages();
 
     var desiredPage = Array.prototype.slice.call(pages[i]);
-
-    //console.log(desiredPage);
 
     desiredPage.forEach(function(divForDisplay){
         divForDisplay.style.display = "";
