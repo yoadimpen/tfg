f6f0d2583d4529391dff62809ffa56d9d7e687fc
Yoana Penkova <yoadimpen@alum.us.es>
2021-03-02 13:07:15 +0100

    ST015/007: admin settings have been added fully to the demo

--- js/adminFunctions.js
+++ js/adminFunctions.js
@@ -194,3 +194,134 @@ function deleteSettings(){
 
     settingsDiv.appendChild(newBtnDiv);
 }
+
+function showCurrentDemoConfig(){
+    var config = String(localStorage.getItem("demoConfig"));
+
+    if(config != null){
+
+        var config_json = JSON.parse(config);
+        var type = config_json.type;
+
+        if(type == "directory"){
+            var typeElement = document.getElementById("inlineRadio1");
+            typeElement.setAttribute("checked", "true");
+        } else {
+            var typeElement = document.getElementById("inlineRadio2");
+            typeElement.setAttribute("checked", "true");
+        }
+
+        var inputs = document.getElementById("inputs");
+
+        var links = config_json.links;
+        for(i=0;i<links.length;i++){
+            var input = document.createElement("input");
+            input.setAttribute("id", "input".concat(i));
+            input.setAttribute("type", "text");
+            input.setAttribute("class", "form-control");
+            input.setAttribute("placeholder", "file:///X:/Path/To/Report");
+            input.setAttribute("onkeyup", "addSubstractNewInput(".concat(i).concat(")"));
+            input.setAttribute("value", links[i]);
+            input.setAttribute("aria-describedby", "basic-addon1");
+            input.setAttribute("style", "margin-bottom:1.5%;");
+
+            inputs.appendChild(input);
+        }
+    } else {
+        showEmptyInput();
+    }
+}
+
+function saveDemoSettings(){
+    var type;
+    var links = ["", "", "", "", "", "", "", "", "", ""];
+
+    var typeInput = document.getElementById("inlineRadio1");
+    
+    if (typeInput.checked) {
+        type = "directory";
+    } else {
+        type = "specific_files";
+    }
+
+    for(i=0;i<10;i++){
+        var id = "input".concat(i);
+
+        var posInput = document.getElementById(id);
+
+        if(typeof(posInput) != 'undefined' && posInput != null){
+            links[i] = posInput.value.trim();
+        }
+    }
+
+    console.log(links);
+
+    var json_links = '\"links\": [\"' + links[0];
+
+    for(j=1;j<10;j++){
+        if(links[j].trim() != ""){
+            json_links = json_links.concat('\", \"' + links[j]);
+        }
+    }
+
+    json_links = json_links.concat('\"]' + ' }');
+
+    var json = '{ ' + '\"type\": ' + '\"' + type + '\", ' + json_links;
+
+    localStorage.setItem("demoConfig", json);
+
+    var settingsDiv = document.getElementById("btns-settings");
+
+    var newBtnDiv = document.getElementById("successMsg");
+
+    if(typeof(newBtnDiv) != 'undefined' && newBtnDiv != null){
+        newBtnDiv.innerHTML = "";
+    } else {
+        newBtnDiv = document.createElement("div");
+        newBtnDiv.setAttribute("class", "col-4");
+        newBtnDiv.setAttribute("id", "successMsg");
+        newBtnDiv.setAttribute("style", "margin-top: 3%;");
+    }
+
+    var successButton = document.createElement("button");
+    successButton.setAttribute("class", "btn btn-success");
+    successButton.setAttribute("disabled", "true");
+    successButton.setAttribute("type", "button");
+    successButton.setAttribute("id", "button-addon2");
+    successButton.appendChild(document.createTextNode("Your configuration has been saved successfully!"));
+
+    newBtnDiv.appendChild(successButton);
+
+    settingsDiv.appendChild(newBtnDiv);
+}
+
+function deleteDemoSettings(){
+    if(localStorage.getItem("demoConfig")!=null){
+        localStorage.removeItem("demoConfig");
+    }
+    showEmptyInput();
+
+    var settingsDiv = document.getElementById("btns-settings");
+
+    var newBtnDiv = document.getElementById("successMsg");
+
+    if(typeof(newBtnDiv) != 'undefined' && newBtnDiv != null){
+        newBtnDiv.innerHTML = "";
+    } else {
+        newBtnDiv = document.createElement("div");
+        newBtnDiv.setAttribute("class", "col-8");
+        newBtnDiv.setAttribute("id", "successMsg");
+        newBtnDiv.setAttribute("style", "margin-top: 3%;");
+    }
+
+    var successButton = document.createElement("button");
+    successButton.setAttribute("class", "btn btn-success");
+    successButton.setAttribute("disabled", "true");
+    successButton.setAttribute("type", "button");
+    successButton.setAttribute("id", "button-addon2");
+    successButton.appendChild(document.createTextNode("Your configuration has been deleted successfully! You're free to enter some new values."));
+
+    newBtnDiv.appendChild(successButton);
+
+    settingsDiv.appendChild(newBtnDiv);
+}
