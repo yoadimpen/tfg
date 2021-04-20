490a7f44d6a43c32998070e2d98bff4098358fc9
Yoana Penkova <yoadimpen@alum.us.es>
2021-04-20 16:37:30 +0200

    ST019/010: validation error resolved

--- js/summaryFunctions.js
+++ js/summaryFunctions.js
@@ -806,7 +806,7 @@ function isFree(name){
 
     if(customCharts != null){
         customCharts.graphs.forEach(function(graph){
-            res = res && (toLowerCase(graph.name) != toLowerCase(name));
+            res = res && (graph.name.toLowerCase() != name.toLowerCase());
         })
     }
 
